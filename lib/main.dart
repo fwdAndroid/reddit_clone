66750d4a63af8ef18e85db1919b57c125a34dbb1
fwdAndroid <fwdkaleem@gmail.com>
2022-10-21 01:14:45 +0500

    Auth Section Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:reddit_clone/screen/auth/login_screen.dart';
+import 'package:reddit_clone/screen/auth_section/screen/login_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
