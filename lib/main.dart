0e18799ec6e95975255de9287d06d6da826b991d
fwdAndroid <fwdkaleem@gmail.com>
2022-10-21 16:08:12 +0500

    Routes+Theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_clone/router.dart';
 import 'package:reddit_clone/screen/screen/login_screen.dart';
+import 'package:reddit_clone/widgets/theme.dart';
+import 'package:routemaster/routemaster.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,22 +20,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const LoginScreen(),
-    );
+    return MaterialApp.router(
+        title: 'Flutter Demo',
+        routerDelegate: RoutemasterDelegate(
+            routesBuilder: (BuildContext context) => loggedOutRoute),
+        routeInformationParser: const RoutemasterParser(),
+        theme: Pallete.darkModeAppTheme);
   }
 }
 
