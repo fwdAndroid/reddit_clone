9ad0a70cb36f22c3dd1e656a4ffd94c8bffb988c
fwdAndroid <fwdkaleem@gmail.com>
2022-10-21 17:33:04 +0500

    Creating Communities and Storing to Firebase

--- lib/screen/screen/create_community_screen.dart
+++ lib/screen/screen/create_community_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit_clone/app_functions_constructor/responsive.dart';
+import 'package:reddit_clone/screen/controllers/community_controller.dart';
+import 'package:reddit_clone/widgets/loader.dart';
 
 class CreateCommunityScreen extends ConsumerStatefulWidget {
   const CreateCommunityScreen({super.key});
@@ -19,60 +21,62 @@ class _CreateCommunityScreenState extends ConsumerState<CreateCommunityScreen> {
     communityNameController.dispose();
   }
 
-  // void createCommunity() {
-  //   ref.read(communityControllerProvider.notifier).createCommunity(
-  //         communityNameController.text.trim(),
-  //         context,
-  //       );
-  // }
+  void createCommunity() {
+    ref.read(communityControllerProvider.notifier).createCommunity(
+          communityNameController.text.trim(),
+          context,
+        );
+  }
 
   @override
   Widget build(BuildContext context) {
-    // final isLoading = ref.watch(communityControllerProvider);
+    final isLoading = ref.watch(communityControllerProvider);
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Create a community'),
       ),
-      body: Responsive(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: [
-              const Align(
-                alignment: Alignment.topLeft,
-                child: Text('Community name'),
-              ),
-              const SizedBox(height: 10),
-              TextField(
-                controller: communityNameController,
-                decoration: const InputDecoration(
-                  hintText: 'r/Community_name',
-                  filled: true,
-                  border: InputBorder.none,
-                  contentPadding: EdgeInsets.all(18),
+      body: isLoading
+          ? Loader()
+          : Responsive(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    const Align(
+                      alignment: Alignment.topLeft,
+                      child: Text('Community name'),
+                    ),
+                    const SizedBox(height: 10),
+                    TextField(
+                      controller: communityNameController,
+                      decoration: const InputDecoration(
+                        hintText: 'r/Community_name',
+                        filled: true,
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.all(18),
+                      ),
+                      maxLength: 21,
+                    ),
+                    const SizedBox(height: 30),
+                    ElevatedButton(
+                      onPressed: createCommunity,
+                      style: ElevatedButton.styleFrom(
+                          minimumSize: const Size(double.infinity, 50),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          )),
+                      child: const Text(
+                        'Create community',
+                        style: TextStyle(
+                          fontSize: 17,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-                maxLength: 21,
               ),
-              const SizedBox(height: 30),
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                    minimumSize: const Size(double.infinity, 50),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    )),
-                child: const Text(
-                  'Create community',
-                  style: TextStyle(
-                    fontSize: 17,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+            ),
     );
   }
 }
